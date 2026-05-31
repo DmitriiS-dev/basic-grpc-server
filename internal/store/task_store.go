926724d721c2c171f6358dc4ffd14d2145249056
Dmitrii Sizikov <dmitrii.sizikov@sita.aero>
2026-05-31 01:19:12 +0100

    merge remote and local

---
+++ internal/store/task_store.go
@@ -0,0 +1,38 @@
+package store
+
+import (
+	pb "github.com/DmitriiS-dev/basic-backend/proto"
+	"sync"
+)
+
+var (
+	tasks = make([]*pb.Task, 0)
+	mu    sync.Mutex
+)
+
+func AddTask(t *pb.Task) {
+	mu.Lock()
+	defer mu.Unlock()
+	tasks = append(tasks, t)
+}
+
+func GetTasks() []*pb.Task {
+	mu.Lock()
+	defer mu.Unlock()
+
+	out := make([]*pb.Task, len(tasks))
+	copy(out, tasks)
+	return out
+}
+
+func DeleteTask(id int32) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for i, task := range tasks {
+		if task.Id == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+		}
+	}
+
+}
